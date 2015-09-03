8557edf282e7980a0ca34221cf298ed289678dd4
Mickael Guene <mickael.guene@st.com>
2015-09-03 08:21:24 +0200

    Better if we uncomment build process ....

--- aci/aci.sh
+++ aci/aci.sh
@@ -8,9 +8,9 @@ TOP=`pwd`
 JOB_NAME=`echo $1`
 isDelivery=`echo ${JOB_NAME} | grep delivery` || true
 
-#${TOP}/scratch/build/scripts/build.sh ${JOB_NAME}
-#${TOP}/scratch/build/scripts/build_runtime.sh ${JOB_NAME}
-#${TOP}/scratch/build/scripts/sanity.sh
+${TOP}/scratch/build/scripts/build.sh ${JOB_NAME}
+${TOP}/scratch/build/scripts/build_runtime.sh ${JOB_NAME}
+${TOP}/scratch/build/scripts/sanity.sh
 if [ $isDelivery ]; then
     ${TOP}/scratch/build/aci/delivery.sh
 fi
